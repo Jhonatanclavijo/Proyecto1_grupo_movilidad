6c1089c1dd2a226bfad4fcadfd492a77920d641a
Jhonatanclavijo <j.clavijog@uniandes.edu.co>
2026-09-11 20:24:01 -0500

    cargue de datos

---
+++ Jhonatan_P1/Archivo_inicial_J.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd 
+
+df = pd.read_csv(r"C:\Users\jhona\OneDrive - Universidad de los Andes\2026-2\Analítica Para la toma de Desiciones\Proyecto 1\Repositorio no Tocar\Datos_completos_SDM.csv")
+
+print(df.head())
